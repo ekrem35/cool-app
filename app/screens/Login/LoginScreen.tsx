17f3f81ebf4c418350a03fd71d337a63c73ab360
ekrem35 <anliekrem35@gmail.com>
2021-05-25 22:59:48 +0300

    login screen ui created

--- app/screens/Login/LoginScreen.tsx
+++ app/screens/Login/LoginScreen.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Container, Content, Text} from 'native-base';
+import {Image} from 'react-native';
+import {
+  Container,
+  Content,
+  Input,
+  Label,
+  Item,
+  Button,
+  Text,
+} from 'native-base';
 import LinearGradient from 'react-native-linear-gradient';
+import styles from './styles';
+const LogoWhite = require('../../images/logo-white.png');
 
 const Login = () => {
   return (
     <Container>
-      <Content>
-        <Text>login.js</Text>
-      </Content>
+      <LinearGradient
+        colors={['#f0645d', '#ee205f']}
+        end={{x: 1, y: 0}}
+        start={{x: 0, y: 0}}
+        style={styles.linearGradientBackground}>
+        <Content contentContainerStyle={styles.content}>
+          <Image style={styles.logo} source={LogoWhite} />
+          <Item style={styles.userNameItem} floatingLabel>
+            <Label style={styles.inputTitle}>Username</Label>
+            <Input style={styles.input} />
+          </Item>
+          <Item style={styles.passwordItem} floatingLabel>
+            <Label style={styles.inputTitle}>Password</Label>
+            <Input style={styles.input} />
+          </Item>
+          <Button transparent style={styles.signInButton} bordered full>
+            <Text style={styles.buttonText}>Sign in</Text>
+          </Button>
+        </Content>
+      </LinearGradient>
     </Container>
   );
 };
---
+++ app/screens/Login/styles.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  linearGradientBackground: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 40,
+    borderRadius: 6,
+    justifyContent: 'center',
+  },
+  logo: {height: 60, width: 160},
+  userNameItem: {marginLeft: 0, marginTop: 48},
+  passwordItem: {marginLeft: 0, marginTop: 16},
+  signInButton: {marginTop: 48, borderRadius: 6, borderColor: '#fff'},
+  buttonText: {color: '#fff'},
+  inputTitle: {color: '#fff'},
+  input: {color: '#fff'},
+});
