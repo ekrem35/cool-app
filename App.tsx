6db070c98a0ec90056cf67ec915539a0fc77f0d5
ekrem35 <anliekrem35@gmail.com>
2021-05-27 03:34:58 +0300

    statusbar style updates

--- App.tsx
+++ App.tsx
@@ -7,10 +7,12 @@ import {Root} from 'native-base';
 import Router from './app/router';
 import {Provider} from 'react-redux';
 import store from './app/state/store';
+import {StatusBar} from 'react-native';
 
 const App = () => {
   return (
     <Root>
+      <StatusBar translucent backgroundColor="transparent" />
       <Provider store={store}>
         <Router />
       </Provider>
