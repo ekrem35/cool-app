e7b0ff189c8043ad0e69174a96e9f1075a97a08e
ekrem35 <anliekrem35@gmail.com>
2021-05-27 03:35:35 +0300

    header  and logut actions added

--- app/screens/MainScreen/MainScreen.tsx
+++ app/screens/MainScreen/MainScreen.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import {Container, Content, Text} from 'native-base';
+import {TouchableOpacity} from 'react-native';
+import {
+  Body,
+  Container,
+  Content,
+  Text,
+  Header,
+  Title,
+  Icon,
+  Right,
+} from 'native-base';
+import styles from './styles';
+import {useDispatch} from 'react-redux';
+import {logout} from '../../state/actions/user';
+import {Actions} from 'react-native-router-flux';
 
-const Login = () => {
+const MainScreen = () => {
+  const dispatch = useDispatch();
+  const onPressLogout = () => {
+    dispatch(logout());
+    Actions.login();
+  };
+
   return (
     <Container>
+      <Header style={styles.header} androidStatusBarColor="#ee205f">
+        <Body>
+          <Title>Cool App</Title>
+        </Body>
+        <Right style={styles.headerRight}>
+          <TouchableOpacity onPress={onPressLogout}>
+            <Icon style={styles.logoutIcon} type="AntDesign" name="logout" />
+          </TouchableOpacity>
+        </Right>
+      </Header>
       <Content>
         <Text>main screen</Text>
       </Content>
@@ -11,4 +41,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default MainScreen;
---
+++ app/screens/MainScreen/styles.tsx
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  menuIcon: {color: 'white', fontSize: 22},
+  headerRight: {maxWidth: 48},
+  logoutIcon: {color: 'white', fontSize: 22},
+  header: {backgroundColor: '#ee205f', marginTop: 16},
+});
